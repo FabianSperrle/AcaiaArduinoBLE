9ffcd1813024fcbe633f5480332b31e0165bd0d9
Andreas Tacke <at@fiendie.net>
2025-07-16 12:28:09 +0200

    Refactor scale connection handling to make it non-blocking
    - Replace blocking do-while loop in init() with state machine
    - Add ConnectionState enum and updateConnection() method
    - Add isConnecting() helper method
    - Maintain 10-second timeout but allow other code to run during connection
    
    Disclaimer: This breaks the current initialization flow because init()
    returns immediately and updateConnection() has to be called from the
    main loop

--- AcaiaArduinoBLE.h
+++ AcaiaArduinoBLE.h
@@ -24,16 +24,28 @@
 #include "Arduino.h"
 #include <ArduinoBLE.h>
 
-enum scale_type{
+enum scale_type {
     OLD,    // Lunar (pre-2021)
     NEW,    // Lunar (2021), Pyxis
     GENERIC // Felicita Arc, etc
 };
 
-class AcaiaArduinoBLE{
+enum ConnectionState {
+    IDLE,
+    SCANNING,
+    CONNECTING,
+    DISCOVERING,
+    CONFIGURING,
+    CONNECTED,
+    FAILED
+};
+
+class AcaiaArduinoBLE {
     public:
         AcaiaArduinoBLE(bool debug);
         bool init(String = "");
+        bool updateConnection();
+        bool isConnecting();
         bool tare();
         bool startTimer();
         bool stopTimer();
@@ -43,6 +55,7 @@ class AcaiaArduinoBLE{
         bool heartbeatRequired();
         bool isConnected();
         bool newWeightAvailable();
+
     private:
         bool isScaleName(String);
 
@@ -58,9 +71,14 @@ class AcaiaArduinoBLE{
         long                _lastHeartBeat;
         bool                _connected;
         scale_type          _type;
-        bool                _debug; 
+        bool                _debug;
         long                _packetPeriod;
         long                _lastPacket;
+
+        ConnectionState     _connectionState;
+        unsigned long       _connectionStartTime;
+        String              _targetMac;
+        BLEDevice           _foundPeripheral;
 };
 
 #endif
