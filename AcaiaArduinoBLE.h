8bd00491ab1f6378cc5d081fb12f0cd2574f6ddc
Andreas Tacke <at@fiendie.net>
2025-07-27 17:47:16 +0200

    Clean up scanning results to avoid memory issues

--- AcaiaArduinoBLE.h
+++ AcaiaArduinoBLE.h
@@ -115,6 +115,7 @@ class AcaiaArduinoBLE {
         static void staticNotifyCallback(NimBLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify);
         void notifyCallback(const uint8_t* pData, size_t length);
         void cleanup();
+        void clearScanResults();
 
         // Debug functions
         void printData(const uint8_t data[], size_t length);
@@ -140,6 +141,7 @@ class AcaiaArduinoBLE {
         unsigned long _connectionStartTime;
         String _targetMac;
         bool _cleanupComplete;
+        unsigned long _lastScanClear;
 
         int _connectionAttempts;
 
